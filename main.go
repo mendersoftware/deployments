7abba081bdba96bea644f7d8536291b7a7d96253
Maciej Mrowiec <mrowiec.maciej@gmail.com>
2015-11-25 18:29:41 +0100

    Build binaries with build information injected in variables.
    Inject:
    * Commit
    * Tag
    * Branch
    * BuildNumber

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+var (
+	Commit      string
+	Tag         string
+	Branch      string
+	BuildNumber string
+)
+
 func main() {
-	fmt.Println("Hello Artefacts!")
+	fmt.Println("Hello Artifacts!")
+
+	fmt.Println("Commit =", Commit)
+	fmt.Println("Tag =", Tag)
+	fmt.Println("Branch =", Branch)
+	fmt.Println("BuildNumber =", BuildNumber)
 }
