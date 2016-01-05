2c5e19e30433f7c60eb829c595e7ebae19e6a0e2
Maciej Mrowiec <mrowiec.maciej@gmail.com>
2016-01-05 14:40:46 +0100

    Refactor routing.go to simplify code.

--- routing.go
+++ routing.go
@@ -5,34 +5,35 @@ import (
 	"github.com/mendersoftware/artifacts/config"
 	"github.com/mendersoftware/artifacts/controllers"
 	"github.com/mendersoftware/artifacts/handlers"
+	"github.com/mendersoftware/artifacts/models/fileservice"
 	"github.com/mendersoftware/artifacts/models/fileservice/s3"
 	"github.com/mendersoftware/artifacts/models/images/memmap"
 	"github.com/mendersoftware/artifacts/utils/safemap"
 )
 
-// NewRouter defines all REST API routes.
-func NewRouter(c config.ConfigReader) (rest.App, error) {
-
-	images := memmap.NewImagesInMem(safemap.NewStringMap())
+func SetupFileStorage(c config.ConfigReader) fileservice.FileServiceModelI {
 
 	bucket := c.GetString(SettingAweS3Bucket)
 	region := c.GetString(SettingAwsS3Region)
 
-	var fileStorage *s3.SimpleStorageService
-
 	if c.IsSet(SettingsAwsAuth) {
-		fileStorage = s3.NewSimpleStorageServiceStatic(
+		return s3.NewSimpleStorageServiceStatic(
 			bucket,
 			c.GetString(SettingAwsAuthKeyId),
 			c.GetString(SettingAwsAuthSecret),
 			region,
 			c.GetString(SettingAwsAuthToken),
 		)
-	} else {
-		fileStorage = s3.NewSimpleStorageServiceDefaults(bucket, region)
 	}
 
-	meta := handlers.NewImageMeta(controllers.NewImagesController(images, fileStorage))
+	return s3.NewSimpleStorageServiceDefaults(bucket, region)
+}
+
+// NewRouter defines all REST API routes.
+func NewRouter(c config.ConfigReader) (rest.App, error) {
+
+	images := memmap.NewImagesInMem(safemap.NewStringMap())
+	meta := handlers.NewImageMeta(controllers.NewImagesController(images, SetupFileStorage(c)))
 
 	app, err := rest.MakeRouter(
 		rest.Get("/api/0.0.1/images", meta.Lookup),
