1e626fddc09e34210ba9e4412eb1266a869d0d04
Maciej Mrowiec <mrowiec.maciej@gmail.com>
2016-01-05 14:16:20 +0100

    Add test for handling config files.
    Checks also default config file which is a big plus as it will fail the
    test if there will be errors.

--- main_test.go
+++ main_test.go
@@ -1,1 +1,16 @@
 package main
+
+import "testing"
+
+func TestHandleConfigFile(t *testing.T) {
+
+	if _, err := HandleConfigFile(""); err == nil {
+		t.FailNow()
+	}
+
+	// Depends on default config being avaiable and correct (which is nice!)
+	if _, err := HandleConfigFile("config.yaml"); err != nil {
+		t.FailNow()
+	}
+
+}
