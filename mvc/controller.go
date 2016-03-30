e1d915e1a10e05b415db1c3b744f4df0b6178c92
Maciej Mrowiec <mrowiec.maciej@gmail.com>
2016-03-30 12:03:45 +0200

    Add MVC helper package for building RESTful API
    Includes common controllers and views for controller methods.
    
    Controller managed view/models and performs actions dependent on http
    method.
    
    View is responsible for output presentation: serialising payload to
    body, setting appropriate headers.
    
    Defines Model (domain model) interfaces that can be used with
    controllers.
    
    Tests are going to be added later.

---
+++ mvc/controller.go
@@ -0,0 +1,57 @@
+// Copyright 2016 Mender Software AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+package mvc
+
+import (
+	"net/http"
+
+	"github.com/ant0ine/go-json-rest/rest"
+)
+
+func NewCreateController(model CreateModeler, view Viewer) rest.HandlerFunc {
+	return func(w rest.ResponseWriter, r *rest.Request) {
+		// Validate incomming request
+		obj := model.NewObject()
+
+		if err := r.DecodeJsonPayload(&obj); err != nil {
+			view.RenderError(w, err, http.StatusBadRequest)
+			return
+		}
+
+		if err := model.Validate(obj); err != nil {
+			view.RenderError(w, err, http.StatusBadRequest)
+			return
+		}
+
+		id, err := model.Create(obj)
+		if err != nil {
+			view.RenderError(w, err, http.StatusInternalServerError)
+			return
+		}
+
+		view.RenderSuccess(w, id)
+	}
+}
+
+func NewFindOneController(model FindOneModeler, view Viewer) rest.HandlerFunc {
+	return func(w rest.ResponseWriter, r *rest.Request) {
+		response, err := model.FindOne(r.PathParam("id"))
+		if err != nil {
+			view.RenderError(w, err, http.StatusInternalServerError)
+			return
+		}
+
+		view.RenderSuccess(w, response)
+	}
+}
