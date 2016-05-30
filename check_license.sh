999db41651c98106fe5a32bd1b27146ca50d44bf
Maciej Mrowiec <mrowiec.maciej@gmail.com>
2016-05-30 12:50:35 +0200

    Adjust check_license.sh to ignore vendor directory instead Godep
    Signed-off-by: Maciej Mrowiec <mrowiec.maciej@gmail.com>

--- check_license.sh
+++ check_license.sh
@@ -2,7 +2,7 @@
 lines=$(cat LICENSE | wc -l)
 
 # failures=0
-for each in $(find . -type f \( ! -regex '.*/\..*' ! -path "./Godeps/*" -name '*.go' \)); do
+for each in $(find . -type f \( ! -regex '.*/\..*' ! -path "./vendor/*" -name '*.go' \)); do
   echo "Checking $each for correct license header"
   head -n $lines $each | diff -qu LICENSE - > /dev/null
   if [ ! "$?" -eq "0" ]; then
