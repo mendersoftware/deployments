d7bec7d46e507a46bb8df85e1768f72cb8ef1b78
Maciej Borzecki <maciej.borzecki@rndity.com>
2016-07-29 11:08:53 +0200

    deployments/mongo: verify that deployment log is present in DB
    Signed-off-by: Maciej Borzecki <maciej.borzecki@rndity.com>

--- resources/deployments/mongo/device_deployment_logs_test.go
+++ resources/deployments/mongo/device_deployment_logs_test.go
@@ -16,11 +16,22 @@ package mongo_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/mendersoftware/deployments/resources/deployments"
 	. "github.com/mendersoftware/deployments/resources/deployments/mongo"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/mgo.v2/bson"
 )
+
+func parseTime(t *testing.T, value string) *time.Time {
+	tm, err := time.Parse(time.RFC3339, value)
+	if assert.NoError(t, err) == false {
+		t.Fatalf("failed to parse time %s", value)
+	}
+
+	return &tm
+}
 
 func TestSaveDeviceDeploymentLog(t *testing.T) {
 
@@ -52,10 +63,33 @@ func TestSaveDeviceDeploymentLog(t *testing.T) {
 		},
 		{
 			InputDeviceDeploymentLog: &deployments.DeploymentLog{
-				Messages: []deployments.LogMessage{},
+				DeviceID:     "456",
+				DeploymentID: "30b3e62c-9ec2-4312-a7fa-cff24cc7397a",
+				Messages:     []deployments.LogMessage{},
 			},
 			InputDeviceID:     "456",
 			InputDeploymentID: "30b3e62c-9ec2-4312-a7fa-cff24cc7397a",
+			OutputError:       nil,
+		},
+		{
+			InputDeviceDeploymentLog: &deployments.DeploymentLog{
+				DeviceID:     "567",
+				DeploymentID: "30b3e62c-9ec2-4312-a7fa-cff24cc7397b",
+				Messages: []deployments.LogMessage{
+					{
+						Level:     "notice",
+						Message:   "foo",
+						Timestamp: parseTime(t, "2006-01-02T15:04:05-07:00"),
+					},
+					{
+						Level:     "notice",
+						Message:   "bar",
+						Timestamp: parseTime(t, "2006-01-02T15:05:05-07:00"),
+					},
+				},
+			},
+			InputDeviceID:     "567",
+			InputDeploymentID: "30b3e62c-9ec2-4312-a7fa-cff24cc7397b",
 			OutputError:       nil,
 		},
 	}
@@ -79,8 +113,21 @@ func TestSaveDeviceDeploymentLog(t *testing.T) {
 			assert.EqualError(t, err, testCase.OutputError.Error())
 		} else {
 			assert.NoError(t, err)
-		}
 
+			// no errors, so we should be able to find the log in DB
+			var dlog deployments.DeploymentLog
+			err := session.DB(DatabaseName).C(CollectionDeviceDeploymentLogs).Find(bson.M{
+				StorageKeyDeviceDeploymentDeviceId:     testCase.InputDeviceID,
+				StorageKeyDeviceDeploymentDeploymentID: testCase.InputDeploymentID,
+			}).One(&dlog)
+
+			assert.NoError(t, err)
+
+			// message timestamp is a pointer, so we cannot use assert.EqualValues()
+			// or reflect.DeepEqual() as both will choke on *time.Time pointing to
+			// different, but value-equal instances, just compare if length is ok for now
+			assert.Len(t, dlog.Messages, len(testCase.InputDeviceDeploymentLog.Messages))
+		}
 		// Need to close all sessions to be able to call wipe at next test case
 		session.Close()
 	}
