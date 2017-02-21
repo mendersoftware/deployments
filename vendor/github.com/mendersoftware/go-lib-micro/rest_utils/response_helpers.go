a992fdae7e1b2ed1606e7758a9b79f2875e1e20f
Krzysztof Jaskiewicz <krzysiekjaskiewicz@gmail.com>
2017-02-21 10:27:59 +0100

    update go-lib-micro; add rest_utils and strings packages from go-lib-micro
    Signed-off-by: Krzysztof Jaskiewicz <krzysiekjaskiewicz@gmail.com>

---
+++ vendor/github.com/mendersoftware/go-lib-micro/rest_utils/response_helpers.go
@@ -0,0 +1,50 @@
+// Copyright 2016 Mender Software AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+package rest_utils
+
+import (
+	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/mendersoftware/go-lib-micro/log"
+	"github.com/mendersoftware/go-lib-micro/requestid"
+	"github.com/pkg/errors"
+	"net/http"
+)
+
+// return selected http code + error message directly taken from error
+// log error
+func RestErrWithLog(w rest.ResponseWriter, r *rest.Request, l *log.Logger, e error, code int) {
+	RestErrWithLogMsg(w, r, l, e, code, e.Error())
+}
+
+// return http 500, with an "internal error" message
+// log full error
+func RestErrWithLogInternal(w rest.ResponseWriter, r *rest.Request, l *log.Logger, e error) {
+	msg := "internal error"
+	e = errors.Wrap(e, msg)
+	RestErrWithLogMsg(w, r, l, e, http.StatusInternalServerError, msg)
+}
+
+// return an error code with an overriden message (to avoid exposing the details)
+// log full error
+func RestErrWithLogMsg(w rest.ResponseWriter, r *rest.Request, l *log.Logger, e error, code int, msg string) {
+	w.WriteHeader(code)
+	err := w.WriteJson(map[string]string{
+		rest.ErrorFieldName: msg,
+		"request_id":        requestid.GetReqId(r),
+	})
+	if err != nil {
+		panic(err)
+	}
+	l.F(log.Ctx{}).Error(errors.Wrap(e, msg).Error())
+}
