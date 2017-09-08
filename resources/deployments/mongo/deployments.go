1080d941aaad7b456b7f03ef5d84143ff2fbc957
mchalczynski <marcin.chalczynski@rndity.com>
2017-09-08 12:27:57 +0200

    deployments/mongo: expose/refactor ensureIndexing
    expose the ensureIndex in two versions:
    1. accepts a context, possibly with tenant id - for dealing with
    runtime requests where middleware sets up the context properly
    2. accepts the full db name directly - for migrations, where it's more convenient
    
    version 2 was introduced because a critical bug was observed in *all*
    other services' migration code. typically the Migrations expect a context
    set up just like our middlewares would do it (and use ...DbFromContext).
    this doesn't work because nobody sets up this context - both store.Migrate and
    migrator.Apply work directly on full db names (strings).
    
    this omission is an error, but also - setting up the context when we have a db
    name ready doesn't make any sense. hence the split - Migrations will be set up
    with the db name directly, and use ver 2 of the indexing method.
    
    ps. a set of bugs will be filed for other services, this is critical for MT
    
    Changelog: None
    
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

--- resources/deployments/mongo/deployments.go
+++ resources/deployments/mongo/deployments.go
@@ -74,18 +74,22 @@ func NewDeploymentsStorage(session *mgo.Session) *DeploymentsStorage {
 	}
 }
 
-func (d *DeploymentsStorage) ensureIndexing(ctx context.Context, session *mgo.Session) error {
-	uniqueDeploymentArtifactNameIndex := mgo.Index{
-		Key:    StorageIndexes,
-		Unique: true,
-		Name:   IndexUniqeDeploymentArtifactNameStr,
-		// Build index upfront - make sure this index is allways on.
+func (d *DeploymentsStorage) EnsureIndexing(ctx context.Context, session *mgo.Session) error {
+	db := store.DbFromContext(ctx, DatabaseName)
+
+	return d.DoEnsureIndexing(db, session)
+}
+
+func (d *DeploymentsStorage) DoEnsureIndexing(db string, session *mgo.Session) error {
+	deploymentArtifactNameIndex := mgo.Index{
+		Key:        StorageIndexes,
+		Name:       IndexDeploymentArtifactNameStr,
 		Background: false,
 	}
 
-	return session.DB(store.DbFromContext(ctx, DatabaseName)).
+	return session.DB(db).
 		C(CollectionDeployments).
-		EnsureIndex(uniqueDeploymentArtifactNameIndex)
+		EnsureIndex(deploymentArtifactNameIndex)
 }
 
 // return true if required indexing was set up
@@ -127,7 +131,7 @@ func (d *DeploymentsStorage) Insert(ctx context.Context, deployment *deployments
 	session := d.session.Copy()
 	defer session.Close()
 
-	if err := d.ensureIndexing(ctx, session); err != nil {
+	if err := d.EnsureIndexing(ctx, session); err != nil {
 		return err
 	}
 
