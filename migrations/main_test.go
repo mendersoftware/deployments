75d8c57ae306435b709f1b000984ea2156ca9e0b
mchalczynski <marcin.chalczynski@rndity.com>
2017-09-08 09:41:00 +0200

    migrations/migration_1_2_1: tests
    Changelog: None
    
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

---
+++ migrations/main_test.go
@@ -0,0 +1,39 @@
+// Copyright 2017 Northern.tech AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+package migrations
+
+import (
+	"os"
+	"testing"
+
+	mtesting "github.com/mendersoftware/go-lib-micro/mongo/testing"
+)
+
+var db mtesting.TestDBRunner
+
+// Overwrites test execution and allows for test database setup
+func TestMain(m *testing.M) {
+
+	var status int
+	if !testing.Short() {
+		status = mtesting.WithDB(func(dbtest mtesting.TestDBRunner) int {
+			db = dbtest
+			return m.Run()
+		})
+	} else {
+		status = m.Run()
+	}
+
+	os.Exit(status)
+}
