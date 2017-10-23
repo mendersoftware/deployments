cac4bd7f7f573264a12f9157b1c018cb627cab3a
mchalczynski <marcin.chalczynski@rndity.com>
2017-10-23 15:29:29 +0200

    vendor: update go-lib-micro
    changelog: none
    
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

---
+++ vendor/github.com/mendersoftware/go-lib-micro/rest_utils/api_error.go
@@ -0,0 +1,45 @@
+// Copyright 2017 Northern.tech AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+package rest_utils
+
+import (
+	"encoding/json"
+	"io"
+)
+
+// ApiError wraps errors returned by our APIs
+type ApiError struct {
+	Err   string `json:"error"`
+	ReqId string `json:"request_id"`
+}
+
+func (ae *ApiError) Error() string {
+	return ae.Err
+}
+
+func IsApiError(e error) bool {
+	_, ok := e.(*ApiError)
+	return ok
+}
+
+func ParseApiError(source io.Reader) error {
+	jd := json.NewDecoder(source)
+
+	var aerr ApiError
+	if err := jd.Decode(&aerr); err != nil {
+		return err
+	}
+
+	return &aerr
+}
