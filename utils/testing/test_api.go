6a5245802db8c7b66c6a2e23c4519e68fd5cb61e
mchalczynski <marcin.chalczynski@rndity.com>
2018-04-18 11:16:50 +0200

    utils/testing: factor out common helpers
    add helpers for controller testing:
    - fake api error helper
    - fake api setup method
    
    there's a lot of copy paste of this code, so move it to a separate package at last.
    
    changelog: none
    
    Signed-off-by: mchalczynski <marcin.chalczynski@rndity.com>

---
+++ utils/testing/test_api.go
@@ -0,0 +1,44 @@
+// Copyright 2018 Northern.tech AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+package testing
+
+import (
+	"io/ioutil"
+	"net/http"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/mendersoftware/go-lib-micro/requestid"
+	"github.com/mendersoftware/go-lib-micro/requestlog"
+)
+
+type RouterTypeHandler func(pathExp string, handlerFunc rest.HandlerFunc) *rest.Route
+
+func SetUpTestApi(route string, routeType RouterTypeHandler,
+	handler func(w rest.ResponseWriter, r *rest.Request)) http.Handler {
+
+	router, _ := rest.MakeRouter(routeType(route, handler))
+	api := rest.NewApi()
+	api.Use(
+		&requestlog.RequestLogMiddleware{
+			BaseLogger: &logrus.Logger{Out: ioutil.Discard},
+		},
+		&requestid.RequestIdMiddleware{},
+	)
+	api.SetApp(router)
+
+	rest.ErrorFieldName = "error"
+	return api.MakeHandler()
+
+}
