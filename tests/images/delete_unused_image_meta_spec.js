5c6cf1079a318d2329b5b78e2cbae9746e7ec2b2
Manuel Zedel <manuel.zedel@northern.tech>
2020-03-11 11:19:28 +0100

    tests: removed unused api tests
    Changelog: None
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- tests/images/delete_unused_image_meta_spec.js
+++
@@ -1,8 +0,0 @@
-var frisby = require('frisby');
-var images = require("./images_endpoint.js")
-
-frisby.create('DELETE unused ID results in error')
-    .delete(images.endpoint + '/851675c4-df63-42cf-a33c-cfa471e0524d')
-    .expectStatus(404)
-    .expectHeaderContains('content-type', 'application/json')
-    .toss()
