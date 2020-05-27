bccaf904c8dc3565f1875452e16f16cc8acb34b3
Peter Grzybowski <peter@northern.tech>
2020-05-27 11:25:36 +0200

    Remove unused definitions
    ChangeLog:none
    Signed-off-by: Peter Grzybowski <peter@northern.tech>

--- model/device.go
+++ model/device.go
@@ -41,25 +41,6 @@ type InvDevice struct {
 	UpdatedTs time.Time `json:"updated_ts" bson:"updated_ts,omitempty"`
 }
 
-// Device wrapper
-type Device struct {
-	//system-generated device ID
-	ID string `json:"id" bson:"_id,omitempty"`
-
-	//a map of attributes names and their values.
-	Attributes DeviceAttributes `json:"attributes,omitempty" bson:"attributes,omitempty"`
-
-	//device's group name
-	Group string `json:"-" bson:"group,omitempty"`
-
-	CreatedTs time.Time `json:"-" bson:"created_ts,omitempty"`
-	//Timestamp of the last attribute update.
-	UpdatedTs time.Time `json:"updated_ts" bson:"updated_ts,omitempty"`
-}
-
-// wrapper for device attributes names and values
-type DeviceAttributes map[string]DeviceAttribute
-
 type DeviceIds struct {
 	Devices []string `json:"devices,omitempty" valid:"required" bson:"-"`
 }
