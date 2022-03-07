b3ca5f69567454823a6352b6bd021a6ac9a0bb9e
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2022-03-07 07:15:24 +0100

    chore(security): upgrade mendersoftware/go-lib-micro to the latest
    Ticket: MEN-5468
    Changelog: title
    
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

--- vendor/github.com/mendersoftware/go-lib-micro/mongo/doc/bson.go
+++ vendor/github.com/mendersoftware/go-lib-micro/mongo/doc/bson.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Northern.tech AS
+// Copyright 2022 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -66,6 +66,10 @@ func DocumentFromStruct(
 		field := fields.Field(i)
 		value := s.Field(i)
 		key, valFace, set := valueFromStructField(field, value)
+		if key == "inline" {
+			doc = append(doc, DocumentFromStruct(valFace)...)
+			continue
+		}
 		if set {
 			doc = append(doc, bson.E{Key: key, Value: valFace})
 		}
