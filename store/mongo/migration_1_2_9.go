14ae72173dc55ad83b0261199b30890fed041ae6
Alf-Rune Siqveland <alf.rune@northern.tech>
2022-03-21 10:13:52 +0100

    fix: Freeze devices collection while migration 1.2.9 is ongoing
    changelog: none
    
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- store/mongo/migration_1_2_9.go
+++ store/mongo/migration_1_2_9.go
@@ -15,8 +15,10 @@ package mongo
 
 import (
 	"context"
+	"strings"
 
 	"github.com/mendersoftware/go-lib-micro/mongo/migrate"
+	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 
@@ -28,10 +30,56 @@ type migration_1_2_9 struct {
 	db     string
 }
 
-func (m *migration_1_2_9) Up(from migrate.Version) error {
+func (m *migration_1_2_9) Up(from migrate.Version) (err error) {
+	const (
+		DBNameAdmin       = "admin"
+		CollNameMigration = CollectionDevices + "-1_2_9"
+	)
+	var (
+		CollNamespaceSrc = strings.Join([]string{m.db, CollectionDevices}, ".")
+		CollNamespaceTmp = strings.Join([]string{m.db, CollNameMigration}, ".")
+	)
 	ctx := context.Background()
 	storage := NewDataStoreMongoWithClient(m.client)
-	collDev := storage.client.Database(m.db).Collection(CollectionDevices)
+	database := storage.client.Database(m.db)
+
+	// Rename the collection such that no writes propagate to the
+	// collection under migration.
+	// The first step is to allow reentrant executions of the migration.
+	cur, err := database.ListCollections(ctx, bson.D{{Key: "name", Value: CollNameMigration}})
+	if err != nil {
+		return errors.Wrap(err, "failed to list collection names")
+	}
+	if !cur.Next(ctx) {
+		err = storage.client.
+			Database(DBNameAdmin).
+			RunCommand(ctx, bson.D{
+				{Key: "renameCollection", Value: CollNamespaceSrc},
+				{Key: "to", Value: CollNamespaceTmp},
+				{Key: "dropTarget", Value: false},
+				{Key: "comment", Value: "Freeze collection for migration 1.2.9"},
+			}).Err()
+		if err != nil {
+			return errors.Wrap(err, "failed to freeze collection")
+		}
+	}
+	defer func() {
+		errRestore := storage.client.
+			Database(DBNameAdmin).
+			RunCommand(ctx, bson.D{
+				{Key: "renameCollection", Value: CollNamespaceTmp},
+				{Key: "to", Value: CollNamespaceSrc},
+				{Key: "dropTarget", Value: true},
+				{Key: "comment", Value: "Thaw collection for migration 1.2.9"},
+			}).Err()
+		if err == nil {
+			err = errRestore
+		} else {
+			err = errors.Wrapf(err, "failed to restore collection: %s", errRestore)
+		}
+	}()
+	collDev := storage.client.Database(m.db).Collection(CollNameMigration)
+
 	bulkReqs := []mongo.WriteModel{
 		mongo.NewUpdateManyModel().
 			SetFilter(bson.D{{
@@ -61,7 +109,7 @@ func (m *migration_1_2_9) Up(from migrate.Version) error {
 				Key: StorageKeyDeviceDeploymentActive, Value: false,
 			}}}}),
 	}
-	_, err := collDev.BulkWrite(ctx, bulkReqs)
+	_, err = collDev.BulkWrite(ctx, bulkReqs)
 	if err == nil {
 		err = storage.EnsureIndexes(m.db, CollectionDevices,
 			IndexDeviceDeploymentsActiveCreatedModel,
