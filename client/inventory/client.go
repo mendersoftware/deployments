e4757a3c6ccdfcf6dff32d4e2d70b26cdea019c7
Peter Grzybowski <peter@northern.tech>
2023-04-11 19:53:32 +0200

    chore: go fmt
    Ticket: MEN-6423
    Signed-off-by: Peter Grzybowski <peter@northern.tech>
    (cherry picked from commit d834a25fa48cf4795918e6d9f6a83450cfedf28f)

--- client/inventory/client.go
+++ client/inventory/client.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Northern.tech AS
+// Copyright 2023 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -48,6 +48,7 @@ var (
 )
 
 // Client is the inventory client
+//
 //go:generate ../../utils/mockgen.sh
 type Client interface {
 	CheckHealth(ctx context.Context) error
