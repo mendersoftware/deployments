9bbdc94f6677fdb198e27f9817e9e9bc6ee519c8
Alf-Rune Siqveland <alf.rune@northern.tech>
2023-06-07 10:24:07 +0200

    chore(fix|s3): Only set immutable hostname if using path-style URL
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- storage/s3/options.go
+++ storage/s3/options.go
@@ -1,4 +1,4 @@
-// Copyright 2022 Northern.tech AS
+// Copyright 2023 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -181,7 +181,7 @@ func (opts *Options) toS3Options() (
 			endpointURI := *opts.URI
 			s3Opts.EndpointResolver = s3.EndpointResolverFromURL(endpointURI,
 				func(ep *aws.Endpoint) {
-					ep.HostnameImmutable = true
+					ep.HostnameImmutable = opts.ForcePathStyle
 				},
 			)
 		}
