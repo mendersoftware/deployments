86267984cd46257728cc70f8cdeefd85eae7a302
Peter Grzybowski <peter@northern.tech>
2023-09-29 10:25:54 +0200

    feat: direct upload with skip verify: set metadata on complete
    Changelog: Title
    Ticket: MEN-6696
    Signed-off-by: Peter Grzybowski <peter@northern.tech>

--- app/app_test.go
+++ app/app_test.go
@@ -993,7 +993,7 @@ func TestCompleteUpload(t *testing.T) {
 			defer objStore.AssertExpectations(t)
 			deploy := NewDeployments(ds, objStore, 0, false)
 
-			err := deploy.CompleteUpload(ctx, intentID, tc.SkipVerify)
+			err := deploy.CompleteUpload(ctx, intentID, tc.SkipVerify, nil)
 			tc.ErrorAssertionFunc(t, tc, err)
 		})
 	}
