ca2983668bda404e91383e46270cd501f7e4f98c
Alf-Rune Siqveland <alf.rune@northern.tech>
2024-05-09 08:14:25 +0200

    chore: Infer the CliClient docker compose service from the `host` option
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- tests/common.py
+++ tests/common.py
@@ -328,8 +328,9 @@ class Device:
 
 
 @pytest.fixture(scope="session")
-def cli():
-    return CliClient()
+def cli(request):
+    service = request.config.getoption("host").split(":")[0]
+    return CliClient(service)
 
 
 @pytest.fixture(scope="session")
