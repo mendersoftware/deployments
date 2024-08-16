78fd6ef0daed5a5b422f8db9d3a095e82921a225
Alf-Rune Siqveland <alf.rune@northern.tech>
2024-08-16 15:29:57 +0200

    revert: "fix: db.Find: look for phrases (escape the $text elements)"
    This reverts commit 6936d9b5419b61588e941f8b9f30457cb7ac401d.
    Ticket: MEN-6730
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- store/mongo/datastore_mongo.go
+++ store/mongo/datastore_mongo.go
@@ -2408,7 +2408,7 @@ func (db *DataStoreMongo) Find(ctx context.Context,
 
 		tq := bson.M{
 			"$text": bson.M{
-				"$search": "\"" + match.SearchText + "\"",
+				"$search": match.SearchText,
 			},
 		}
 
